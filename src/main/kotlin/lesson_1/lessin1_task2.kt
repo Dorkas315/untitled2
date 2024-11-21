db0cfce7a2091429f07da57e763bfc53516577ae
Nikita <mikita315msils@gmail.com>
2024-11-21 18:21:24 +0700

    L-t1-2 сделано колличество сотрудников

---
+++ src/main/kotlin/lesson_1/lessin1_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_1
+
+fun main() {
+    val yourOrderNumber = "ваш заказ 75"
+    val customerCommunication = "Благодорим за заказ :>"
+
+    println(yourOrderNumber)
+    println(customerCommunication)
+
+    var sotreStaff = 2000
+//    println(sotreStaff)
+
+    sotreStaff = 1999
+    println(sotreStaff)
+
+
+
+
+}
